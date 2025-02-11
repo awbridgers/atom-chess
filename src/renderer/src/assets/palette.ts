5b38c39aa57103ef7421880971f9ded9c199ed72
Adam Bridgers <16521414+awbridgers@users.noreply.github.com>
2025-02-11 15:26:06 -0500

    change background to test updater

--- src/renderer/src/assets/palette.ts
+++ src/renderer/src/assets/palette.ts
@@ -5,7 +5,7 @@ export const clr : palette = {
   selected: '#FFFC77',
   legal: '#1C313C8A',
   bestMove: '#F57439AA',
-  background: '#02131B',
+  background: '#88131B',
   highlighted: '#D54851CC',
   previousMove: '#FFFC77AA',
   text:'#ffffff'
